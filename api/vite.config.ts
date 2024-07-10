a2157afd05f6bc8846d94f83927a123a65e109dc
Gabriel Francisco <gab9244@gmail.com>
2024-07-10 07:40:45 -0300

    First attempt to deploy

---
+++ api/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
+
+dotenv.config();
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "", // Ensure the trailing slash
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_REACT_APP_API_URL,
+        changeOrigin: true,
+        secure: true,
+      }
+    }
+  }
+})
